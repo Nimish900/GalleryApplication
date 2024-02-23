6e8ed644c2bdb21f7b241d5d9e2cdd5c0c9396a9
Nimish Mothghare <nimishm90@gmail.com>
2024-02-23 17:59:22 +0530

    Initial Commit

---
+++ GalleryApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GalleryApplication
+//
+//  Created by Nimish Mothgare on 23/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
